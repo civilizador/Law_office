d7480fb2358fc1f3c1773753523a27fb7e9ec1df
Asim <salimli.asim@gmail.com>
2018-12-09 16:18:27 +0000

    modal windows added. News section added using bootstrap cards. all news page added.

---
+++ assets/js/script.js
@@ -0,0 +1,69 @@
+function myFunction1() {
+  var dots1 = document.getElementById("dots1");
+  var moreText1 = document.getElementById("more1");
+  var btnText1 = document.getElementById("myBtn1");
+
+  if (dots1.style.display === "none") {
+    dots1.style.display = "inline";
+    moreText1.style.display = "none";
+  } else {
+    dots1.style.display = "none"; 
+    moreText1.style.display = "inline";
+  }
+};
+
+function myFunction2() {
+  var dots2 = document.getElementById("dots2");
+  var moreText2 = document.getElementById("more2");
+  var btnText2 = document.getElementById("myBtn2");
+
+  if (dots2.style.display === "none") {
+    dots2.style.display = "inline";
+    moreText2.style.display = "none";
+  } else {
+    dots2.style.display = "none"; 
+    moreText2.style.display = "inline";
+  }
+};
+
+function myFunction3() {
+  var dots3 = document.getElementById("dots3");
+  var moreText3 = document.getElementById("more3");
+  var btnText3 = document.getElementById("myBtn3");
+
+  if (dots3.style.display === "none") {
+    dots3.style.display = "inline";
+    moreText3.style.display = "none";
+  } else {
+    dots3.style.display = "none"; 
+    moreText3.style.display = "inline";
+  }
+};
+
+function myFunction4() {
+  var dots4 = document.getElementById("dots1");
+  var moreText4 = document.getElementById("more1");
+  var btnText4 = document.getElementById("myBtn1");
+
+  if (dots4.style.display === "none") {
+    dots4.style.display = "inline";
+    moreText4.style.display = "none";
+  } else {
+    dots4.style.display = "none"; 
+    moreText4.style.display = "inline";
+  }
+};
+
+function myFunction5() {
+  var dots5 = document.getElementById("dots5");
+  var moreText5 = document.getElementById("more5");
+  var btnText5 = document.getElementById("myBtn5");
+
+  if (dots5.style.display === "none") {
+    dots5.style.display = "inline";
+    moreText5.style.display = "none";
+  } else {
+    dots5.style.display = "none"; 
+    moreText5.style.display = "inline";
+  }
+}
