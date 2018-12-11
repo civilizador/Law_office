3e85dc77401d16f22f67a83a8605709d33bd4f3f
Asim <salimli.asim@gmail.com>
2018-12-11 03:27:15 +0000

    polishing

--- assets/js/script.js
+++ assets/js/script.js
@@ -68,6 +68,77 @@ function myFunction5() {
   }
 }
 
+             
+             
+function myFunction6() {
+  var dots6 = document.getElementById("dots6");
+  var moreText6 = document.getElementById("more6");
+  var btnText6 = document.getElementById("myBtn6");
+
+  if (dots6.style.display === "none") {
+    dots6.style.display = "inline";
+    moreText6.style.display = "none";
+  } else {
+    dots6.style.display = "none"; 
+    moreText6.style.display = "inline";
+  }
+};
+
+function myFunction7() {
+  var dots7 = document.getElementById("dots7");
+  var moreText7 = document.getElementById("more7");
+  var btnText7 = document.getElementById("myBtn7");
+
+  if (dots7.style.display === "none") {
+    dots7.style.display = "inline";
+    moreText7.style.display = "none";
+  } else {
+    dots7.style.display = "none"; 
+    moreText7.style.display = "inline";
+  }
+};
+
+function myFunction8() {
+  var dots8 = document.getElementById("dots8");
+  var moreText8 = document.getElementById("more8");
+  var btnText8 = document.getElementById("myBtn8");
+
+  if (dots8.style.display === "none") {
+    dots8.style.display = "inline";
+    moreText8.style.display = "none";
+  } else {
+    dots8.style.display = "none"; 
+    moreText8.style.display = "inline";
+  }
+};
+
+function myFunction9() {
+  var dots9 = document.getElementById("dots6");
+  var moreText9 = document.getElementById("more6");
+  var btnText9 = document.getElementById("myBtn6");
+
+  if (dots9.style.display === "none") {
+    dots9.style.display = "inline";
+    moreText9.style.display = "none";
+  } else {
+    dots9.style.display = "none"; 
+    moreText9.style.display = "inline";
+  }
+};
+
+function myFunction10() {
+  var dots10 = document.getElementById("dots10");
+  var moreText10 = document.getElementById("more10");
+  var btnText10 = document.getElementById("myBtn10");
+
+  if (dots10.style.display === "none") {
+    dots10.style.display = "inline";
+    moreText10.style.display = "none";
+  } else {
+    dots10.style.display = "none"; 
+    moreText10.style.display = "inline";
+  }
+}
 
 
 
